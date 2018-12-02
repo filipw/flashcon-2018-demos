b7cdc7f721f21d4a0748b00c35b959e18050dab8
filipw <wojcieszyn@gmail.com>
2018-12-02 12:10:31 +0100

    demos

---
+++ src/CSharpCompiler.Demos.InvalidProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSharpCompiler.Demos.InvalidProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dog = new Dog();
+        }
+    }
+
+
+    struct Animal<T>
+    {
+    }
+
+    struct Dog
+    {
+        public Animal<Dog> Classification { get; set; }
+    }
+}
---
+++ src/CSharpCompiler.Demos.OpenGeneric/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+namespace CSharpCompiler.Demos.OpenGeneric
+{
+    class Program
+    {
+        static void Main()
+        {
+            var normal = Enum.ToObject(typeof(Gift.Color), 0);
+            var weird = Enum.ToObject(typeof(Gift<>.Color), 0);
+            Console.WriteLine(normal.GetType());
+            Console.WriteLine(weird.GetType());
+        }
+    }
+
+    class Gift<T>
+    {
+        public enum Color { }
+    }
+
+    class Gift
+    {
+        public enum Color { }
+    }
+}
